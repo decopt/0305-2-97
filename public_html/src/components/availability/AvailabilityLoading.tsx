5ef6bdb0cd79f9ac26a84ff60a52deaed7c0425b
decopt <decopt10@gmail.com>
2025-06-16 18:24:29 +0100

    Delete public_html directory

--- public_html/src/components/availability/AvailabilityLoading.tsx
+++
@@ -1,8 +0,0 @@
-
-export const AvailabilityLoading = () => {
-  return (
-    <div className="w-full h-full flex items-center justify-center">
-      <p>Carregando horários...</p>
-    </div>
-  );
-};
